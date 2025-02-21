bd71daea56ed5b5cdcb0bad6c04f5b98e2c0d2f3
Mario Saliba <mario.saliba98@gmail.com>
2025-02-21 02:47:11 +0200

    updates migration

--- src/database/migrations/20250219175313_matches.ts
+++ src/database/migrations/20250219175313_matches.ts
@@ -6,9 +6,14 @@ export async function up(knex: Knex): Promise<void> {
     table.string('host_id').notNullable()
     table.string('guest_id')
     table.json('board_data').notNullable()
+    table.json('guest_board_data').notNullable()
     table.string('host_color').notNullable()
     table.string('guest_color').notNullable()
     table.string('turn')
+    table.integer('host_tokens_home')
+    table.integer('guest_tokens_home')
+    table.integer('host_tokens_reached')
+    table.integer('guest_tokens_reached')
   })
 }
 
